b738efd2b448deb0d8e4a8eed6f954a1c47524f2
Sreram K <sreramk@outlook.com>
2018-08-11 12:38:47 +0530

    Still not complted

--- src/matrix/MatrixOppr.java
+++ src/matrix/MatrixOppr.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * copyright (c) 2018 K Sreram, All rights reserved.
  */
 package matrix;
 
@@ -13,8 +11,8 @@ import java.util.ArrayList;
  */
 public class MatrixOppr {
   
-   private ArrayList<ArrayList <Integer>> matrix;
-   private Integer xSize, ySize;
+   private final ArrayList<ArrayList <Integer>> matrix;
+   private final Integer xSize, ySize;
    
    public MatrixOppr (ArrayList<ArrayList <Integer>> pMatrix, Integer pXSize, Integer pYSize) {
        matrix = pMatrix;
@@ -30,7 +28,7 @@ public class MatrixOppr {
    }
    
    public ArrayList<ArrayList <Integer>> getMatrix () {
-       return new ArrayList<ArrayList <Integer>>(matrix);
+       return new ArrayList<>(matrix);
    }
    
    public Integer getElement (int y, int x) {
@@ -64,6 +62,71 @@ public class MatrixOppr {
        }
        return new MatrixOppr (result, newXSize, newYSize);
    }
+   
+   
+   public MatrixOppr minor (int x, int y) {
+       
+       
+       Integer newXSize = this.getXSize() -1;
+       Integer newYSize = this.getYSize() -1;
+       
+       ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+       
+       for (int i = 0, k = 0; i < this.getXSize(); i++ ) {
+           
+           if (i == y) continue;
+           
+           result.add(new ArrayList<>());
+           for (int j = 0; j < this.getYSize(); j++) {
+               if (j != x ) {
+                   result.get(k).add(this.matrix.get(i).get(j));
+               }
+           }
+           k++;
+       }
+       return new MatrixOppr (result, newXSize, newYSize);
+   }
+   
+   public Integer determinant () {
+       
+       if (    (!this.getXSize().equals(this.getYSize())) || 
+               (this.getXSize().equals(0))                ||
+               (this.getYSize().equals(0)) ) {
+           // throw error
+           return null;
+       }
+       
+       if (this.getXSize().compareTo(2) == 0) {
+           return (this.matrix.get(1).get(1) * this.matrix.get(0).get(0) - 
+                   this.matrix.get(1).get(0) * this.matrix.get(0).get(1));
+       }
+       
+       if (this.getXSize().compareTo(1) == 0){
+           return this.matrix.get(0).get(0);
+       }
+       
+       int sign = 1;
+       int sum  = 0;
+       
+       for (int i = 0; i < this.getXSize(); i++) {
+           sum += sign * this.minor(i, 0).determinant();
+           sign = sign * (-1);
+       }
+       
+       return sum;
+   }
+   
+   public MatrixOppr cofactorMatrix () {
+       ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+       for (int i = 0; i < this.getYSize(); i++) {
+           result.add(new ArrayList<>());
+           for (int j = 0; j < this.getXSize(); j++) {
+               result.get(i).add(this.cofactorMatrix().determinant());
+           }
+       }
+       return new MatrixOppr (result, this.getXSize(), this.getYSize());
+   }
+   
    
     
 }
